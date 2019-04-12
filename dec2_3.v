147fd002a553301ec78b5a6e971a0b43c82026f2
yusuke.inoue <yusuke.inoue@cpc.ait.kyushu-u.ac.jp>
2019-04-12 10:05:52 +0900

    fix 2_3decorder to dec2_3.v

--- dec2_3.v
+++ dec2_3.v
@@ -1,4 +1,4 @@
-module 2_3dec{switch, led);
+module dec2_3(switch, led);
    input [1:0] switch; // 2bit
    output [2:0] led;   // 3bit
 
@@ -8,11 +8,11 @@ module 2_3dec{switch, led);
 
       begin
 	 case (dec_in)
-	   2'b00: dec = 4'b000;
-	   2'b01: dec = 4'b001;
-	   2'b10: dec = 4'b010;
-	   2'b11: dec = 4'b100;
-	   default: dec = 4'b000;
+	   2'b00: dec = 3'b000;
+	   2'b01: dec = 3'b001;
+	   2'b10: dec = 3'b010;
+	   2'b11: dec = 3'b100;
+	   default: dec = 3'b000;
 	 endcase // case (dec_in)
       end
    endfunction // dec
